258bfad6c81a621c6f9b37b0c6074cfc45d8d8b2
johannes.wijaya <johannes.wijaya@idexpress.com>
2025-03-05 16:16:57 +0700

    bugfix all related with switch and transaction

--- key.go
+++ key.go
@@ -6,3 +6,4 @@ const SQL_KEY Gosl_Key = 1977
 const CACHE_SQL_KEY Gosl_Key = 1988
 const ACTIVE_SQL_KEY Gosl_Key = 1999
 const SYSTEM_STACK Gosl_Key = 101
+const SYSTEM_CALLBACK_DEPTH Gosl_Key = 102
--- kit.go
+++ kit.go
@@ -3,13 +3,12 @@ package gosl
 import (
 	"context"
 	"errors"
-	"fmt"
 
 	"github.com/jmoiron/sqlx"
 )
 
 type Kit interface {
-	RunInTransaction(ctx context.Context, handler func(ctx context.Context) (context.Context, error)) error
+	RunInTransaction(ctx context.Context, handler func(ctx context.Context) (context.Context, error)) (context.Context, error)
 	ContextSwitch(ctx context.Context, key interface{}) (context.Context, error)
 	ContextReset(ctx context.Context) (context.Context, error)
 }
@@ -25,21 +24,22 @@ type stack struct {
 type kit struct {
 }
 
-func (k *kit) RunInTransaction(ctx context.Context, handler func(ctx context.Context) (context.Context, error)) error {
+func (k *kit) RunInTransaction(ctx context.Context, handler func(ctx context.Context) (context.Context, error)) (context.Context, error) {
 	var err error
 	level := 1
-	stacks, ok := ctx.Value(SYSTEM_STACK).([]stack)
-	if ok && len(stacks) > 0 {
-		level = stacks[len(stacks)-1].Level + 1
+	depth, ok := ctx.Value(SYSTEM_CALLBACK_DEPTH).(int)
+	if ok && depth > 0 {
+		level = depth + 1
 	}
+	ctx = context.WithValue(ctx, SYSTEM_CALLBACK_DEPTH, level)
 	ctx, err = transact(ctx, level)
 	if err != nil {
-		return err
+		return ctx, err
 	}
 	ctx, err = handler(ctx)
-	stacks, ok = ctx.Value(SYSTEM_STACK).([]stack)
+	stacks, ok := ctx.Value(SYSTEM_STACK).([]stack)
 	if !ok {
-		return errors.New("no active transaction")
+		return ctx, errors.New("no active transaction")
 	}
 	if err != nil {
 		for _, stck := range stacks {
@@ -47,24 +47,26 @@ func (k *kit) RunInTransaction(ctx context.Context, handler func(ctx context.Con
 				_ = tx.Rollback()
 			}
 		}
-		return err
+		return ctx, err
 	} else if level == 1 {
 		for _, stck := range stacks {
 			for _, tx := range stck.Transactions {
-				err := tx.Commit()
+				err = tx.Commit()
 				if err != nil {
-					for _, istck := range stacks {
-						for _, itx := range istck.Transactions {
-							_ = itx.Rollback()
-						}
-					}
-					return fmt.Errorf("error when committing transaction: %v", err)
+					break
 				}
 			}
-
+		}
+		if err != nil {
+			for _, stck := range stacks {
+				for _, tx := range stck.Transactions {
+					_ = tx.Rollback()
+				}
+			}
+			return ctx, err
 		}
 	}
-	return nil
+	return ctx, nil
 }
 
 func (k *kit) ContextSwitch(ctx context.Context, key any) (context.Context, error) {
@@ -75,43 +77,37 @@ func (k *kit) ContextSwitch(ctx context.Context, key any) (context.Context, erro
 		keys := make(map[any]any)
 		keys["PRIMARY"] = ctx.Value(SQL_KEY)
 		keys[key] = curr
-		stacks, ok := ctx.Value(SYSTEM_STACK).([]stack)
-		if ok && len(stacks) > 0 {
-			ctx, err = transact(ctx, stacks[len(stacks)-1].Level)
-			if err != nil {
-				return ctx, err
-			}
-		}
 		ctx = context.WithValue(ctx, CACHE_SQL_KEY, keys)
 	} else {
 		keys := cacheKeys.(map[any]any)
 		duplicate := false
-		for ckey, value := range keys {
+		for ckey := range keys {
 			if ckey == key {
 				duplicate = true
-				curr = value
+				break
 			}
 		}
 		if !duplicate {
 			keys[key] = curr
-			stacks, ok := ctx.Value(SYSTEM_STACK).([]stack)
-			if ok && len(stacks) > 0 {
-				ctx, err = transact(ctx, stacks[len(stacks)-1].Level)
-				if err != nil {
-					return ctx, err
-				}
-			}
 		}
 		ctx = context.WithValue(ctx, CACHE_SQL_KEY, keys)
 	}
-	return context.WithValue(ctx, SQL_KEY, curr), nil
+	ctx = context.WithValue(ctx, SQL_KEY, curr)
+	depth, ok := ctx.Value(SYSTEM_CALLBACK_DEPTH).(int)
+	if ok && depth > 0 {
+		ctx, err = transact(ctx, depth)
+		if err != nil {
+			return ctx, err
+		}
+	}
+	return ctx, nil
 }
 
 func (k *kit) ContextReset(ctx context.Context) (context.Context, error) {
 	var err error
-	stacks, ok := ctx.Value(SYSTEM_STACK).([]stack)
-	if ok && len(stacks) > 0 {
-		ctx, err = transact(ctx, stacks[len(stacks)-1].Level)
+	depth, ok := ctx.Value(SYSTEM_CALLBACK_DEPTH).(int)
+	if ok && depth > 0 {
+		ctx, err = transact(ctx, depth)
 		if err != nil {
 			return ctx, err
 		}
@@ -143,10 +139,19 @@ func transact(ctx context.Context, level int) (context.Context, error) {
 			con["tx"] = tx
 			ctx = context.WithValue(ctx, SQL_KEY, NewQueryable(con))
 			stacks, ok := ctx.Value(SYSTEM_STACK).([]stack)
-			if ok && len(stacks) > level-1 && stacks[level-1].Level == level-1 {
-				stacks[level-1].Transactions = append(stacks[level-1].Transactions, tx)
+			found := -1
+			for i := 0; ok && i < len(stacks); i++ {
+				if stacks[i].Level == level {
+					found = i
+					break
+				}
+			}
+			if found != -1 {
+				stacks[found].Transactions = append(stacks[found].Transactions, tx)
 			} else {
-				stacks = make([]stack, 0)
+				if !ok {
+					stacks = make([]stack, 0)
+				}
 				stacks = append(stacks, stack{
 					Level:        level,
 					Transactions: []*sqlx.Tx{tx},
